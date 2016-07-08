8c69f01f14ed3b9f1c7beae373b9465f421de675
Yiyin Shen <Sylvia@Yiyins-MacBook-Pro.local>
2016-07-08 19:07:43 +1000

    dynamically create view controllers

--- CopyWeibo/CopyWeibo/Classes/Main/MainViewController.swift
+++ CopyWeibo/CopyWeibo/Classes/Main/MainViewController.swift
@@ -18,12 +18,38 @@ class MainViewController: UITabBarController {
         
         tabBar.tintColor = UIColor.orangeColor()
         
-        //1. create the main page
-        //let home = HomeTableViewController()
-        addChildViewController(HomeTableViewController(),title:"Home",imageName:"tabbar_home")
-        addChildViewController(DiscoverTableViewController(),title:"Discover",imageName:"tabbar_message_center")
-        addChildViewController(MessageTableViewController(),title:"Message",imageName:"tabbar_discover")
-        addChildViewController(ProfileTableViewController(),title:"Profile",imageName:"tabbar_profile")
+        
+  //      addChildViewController("HomeTableViewController",title:"Home",imageName:"tabbar_home")
+//        addChildViewController(HomeTableViewController(),title:"Home",imageName:"tabbar_home")
+//        addChildViewController(DiscoverTableViewController(),title:"Discover",imageName:"tabbar_message_center")
+//        addChildViewController(MessageTableViewController(),title:"Message",imageName:"tabbar_discover")
+//        addChildViewController(ProfileTableViewController(),title:"Profile",imageName:"tabbar_profile")
+        
+        
+        //create view controllers from the JSON file
+        let path=NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType:nil)
+        if let jsonPath=path{
+            let jsonData=NSData(contentsOfFile: jsonPath)
+            
+            do{
+             let dictionaryArray = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.MutableContainers)
+                print(dictionaryArray)
+                //travase the dictionary to create view controller
+                for vcDictionary in dictionaryArray as! [[String:String]]{
+                    addChildViewController(vcDictionary["vcName"]!, title: vcDictionary["title"]!, imageName: vcDictionary["imageName"]!)
+                
+                }
+                
+                
+                
+            }catch{
+                print(error)
+            
+            }
+           
+        }
+        
+        //
         
         
        
@@ -33,29 +59,50 @@ class MainViewController: UITabBarController {
    
     
     //initialize child view controller
-    private func addChildViewController(childController: UIViewController, title:String, imageName:String){
-    
-    
+//    private func addChildViewController(childController: UIViewController, title:String, imageName:String){
+    private func addChildViewController(childControllerName: String, title:String, imageName:String){
+
+        
+        
+        //1. get name space to create instance
+        let nameSpace = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        let childClass:AnyClass?=NSClassFromString(nameSpace+"."+childControllerName)
+        childClass?.initialize()
+        
+        let vcClass=childClass as! UIViewController.Type
+        let vcInstance=vcClass.init()
+        
+         //2. create tabbar items
+        vcInstance.tabBarItem.image = UIImage(named:imageName)
+        vcInstance.tabBarItem.selectedImage = UIImage(named:imageName+"_highlighted")
+        vcInstance.title = title
+        
+        //3. create navigation bar
+        let navigationBar = UINavigationController()
+        navigationBar.addChildViewController(vcInstance)
+        
+        //4.add navigation bar to the current vewi UITabBarController
+        addChildViewController(navigationBar)
+        
         //1.1 create tabbar items
         
-        childController.tabBarItem.image = UIImage(named:imageName)
-        childController.tabBarItem.selectedImage = UIImage(named:imageName+"_highlighted")
-        
-        
+//        childController.tabBarItem.image = UIImage(named:imageName)
+//        childController.tabBarItem.selectedImage = UIImage(named:imageName+"_highlighted")
+
         //1.2 navigation title
         
-        childController.title = title
+        //childController.title = title
         //will do the following two things at the same tile
         //home.tabBarItem.title = "Home"
         //home.navigationItem.title = "Home"
         
         //2.create the navigation bar
         
-        let navigationBar = UINavigationController()
-        navigationBar.addChildViewController(childController)
+        //let navigationBar = UINavigationController()
+        //navigationBar.addChildViewController(childController)
         
         //3.add navigation bar to the current vewi UITabBarController
-        addChildViewController(navigationBar)
+        //addChildViewController(navigationBar)
     
     }
    
