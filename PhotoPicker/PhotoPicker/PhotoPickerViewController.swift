052c223da674a2ad2c8c2fba55ae751c8ef812d5
Yiyin Shen <Sylvia@Yiyins-MacBook-Pro.local>
2016-08-04 22:34:57 +1000

    PhotoPicker testing project create

---
+++ PhotoPicker/PhotoPicker/PhotoPickerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoPickerViewController.swift
+//  PhotoPicker
+//
+//  Created by Yiyin Shen on 4/08/2016.
+//  Copyright © 2016 Yiyin Shen. All rights reserved.
+//
+
+import Cocoa
+
+class PhotoPickerViewController: UIViewController {
+
+}
---
+++ PhotoPicker/PhotoPickerTests/PhotoPickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoPickerTests.swift
+//  PhotoPickerTests
+//
+//  Created by Yiyin Shen on 4/08/2016.
+//  Copyright © 2016 Yiyin Shen. All rights reserved.
+//
+
+import XCTest
+@testable import PhotoPicker
+
+class PhotoPickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
